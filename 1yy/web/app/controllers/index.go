eb99cfbceb4848ebbab19339f4e6e695cc2a6e48
zhenglixin <lixin.zhenglx@gmail.com>
2015-04-27 23:31:15 +0800

    frontend

--- 1yy/web/app/controllers/index.go
+++ 1yy/web/app/controllers/index.go
@@ -1,20 +1,11 @@
 package controllers
 
-import (
-	"github.com/gocraft/web"
-	"hi.tv/1yy/libs/iqiyi"
-)
+import "github.com/gocraft/web"
 
 type Index struct {
 	*Base
 }
 
 func (c *Index) Index(rw web.ResponseWriter, req *web.Request) {
-	str, err := iqiyi.GetM3u8("http://www.iqiyi.com/v_19rro1vd70.html")
-	if err != nil {
-		c.JSON(500, "GetM3u8 error:"+err.Error())
-		return
-	}
-
-	c.JSON(200, str)
+	c.HTML(200, "index", nil)
 }
